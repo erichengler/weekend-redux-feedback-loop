d3a2e841ca555c094c670ec6ed06e16cfa8e0cf6
Erich Engler <erich.engler@hotmail.com>
2023-04-14 16:39:44 -0500

    part one - feeling - complete. requires user to input a number 1 - 10. next button brings user to understanding page

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,9 +1,40 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import './Feeling.css'
+
 function Feeling () {
 
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const feelingNumber = useSelector(store => store.feeling);
+
+    const handleChange = (event) => {
+        const action = { type: "SET_FEELING", payload: event.target.value };
+        dispatch(action);
+    }
+
+    const nextPage = () => {
+        if ( feelingNumber === 0 || feelingNumber === '' ) {
+            alert('Please enter a number 1 - 10');
+        } else {
+            history.push('/understanding');
+        }
+    }
+    
+
     return (
-        <>
-            <h3>Feeling</h3>
-        </>
+        <div>
+            <h2>How are you feeling today?</h2>
+            <label htmlFor="feeling">Feeling?</label>
+            <br />
+            <input 
+                type="number" 
+                min="1" max="10" 
+                placeholder="1-10"
+                onChange={handleChange} />
+            <button onClick={nextPage}>Next</button>
+        </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,34 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 
+// Imports for redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+
+// Reducers
+const feeling = (state = 0, action) => {
+    if ( action.type === 'SET_FEELING' ) {
+        return action.payload;
+    };
+    return state;
+}
+
+// Store
+const storeInstance = createStore(
+    combineReducers(
+        {
+            feeling,
+        }
+    ),
+    applyMiddleware(logger)
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={storeInstance}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
